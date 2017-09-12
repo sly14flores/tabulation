95a89a98fa6f5ef9ad6c6323de820d5aa9c5837b
Sly Flores <sly@christian.com.ph>
2017-09-12 16:12:45 +0800

    ...

--- tabulation.sql
+++ tabulation.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1
+-- version 4.5.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 16, 2016 at 11:49 AM
--- Server version: 5.6.20
--- PHP Version: 5.4.31
+-- Generation Time: Sep 12, 2017 at 03:59 PM
+-- Server version: 5.7.11
+-- PHP Version: 7.0.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `tabulation`
@@ -26,14 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `consolation_prizes`
 --
 
-CREATE TABLE IF NOT EXISTS `consolation_prizes` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `consolation_prizes` (
+  `id` int(10) NOT NULL,
   `contestant_id` int(10) NOT NULL,
   `overall_score` decimal(10,2) NOT NULL,
-  `place` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `contestant_id` (`contestant_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `place` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -41,15 +39,14 @@ CREATE TABLE IF NOT EXISTS `consolation_prizes` (
 -- Table structure for table `contestants`
 --
 
-CREATE TABLE IF NOT EXISTS `contestants` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `contestants` (
+  `id` int(10) NOT NULL,
   `no` int(10) NOT NULL DEFAULT '0',
   `cluster_name` varchar(100) NOT NULL,
   `leader` varchar(100) NOT NULL,
   `remarks` varchar(100) NOT NULL,
-  `is_active` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `is_active` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contestants`
@@ -73,12 +70,11 @@ INSERT INTO `contestants` (`id`, `no`, `cluster_name`, `leader`, `remarks`, `is_
 -- Table structure for table `criteria`
 --
 
-CREATE TABLE IF NOT EXISTS `criteria` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `criteria` (
+  `id` int(10) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `percentage` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `percentage` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `criteria`
@@ -98,20 +94,12 @@ INSERT INTO `criteria` (`id`, `description`, `percentage`) VALUES
 -- Table structure for table `judges`
 --
 
-CREATE TABLE IF NOT EXISTS `judges` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `judges` (
+  `id` int(10) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `first_name` varchar(100) NOT NULL,
-  `remarks` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `judges`
---
-
-INSERT INTO `judges` (`id`, `last_name`, `first_name`, `remarks`) VALUES
-(1, 'Flores', 'Sly', '');
+  `remarks` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -119,22 +107,21 @@ INSERT INTO `judges` (`id`, `last_name`, `first_name`, `remarks`) VALUES
 -- Table structure for table `preferences`
 --
 
-CREATE TABLE IF NOT EXISTS `preferences` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `preferences` (
+  `id` int(10) NOT NULL,
   `title` varchar(100) NOT NULL,
   `on_going_contestant` int(10) NOT NULL,
   `minimum_score` int(10) NOT NULL,
   `maximum_score` int(10) NOT NULL,
-  `signup_token` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `signup_token` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `preferences`
 --
 
 INSERT INTO `preferences` (`id`, `title`, `on_going_contestant`, `minimum_score`, `maximum_score`, `signup_token`) VALUES
-(1, 'Sayaw Awit', 0, 5, 10, 161216);
+(1, 'Sayaw Awit', 0, 5, 10, 12345);
 
 -- --------------------------------------------------------
 
@@ -142,17 +129,13 @@ INSERT INTO `preferences` (`id`, `title`, `on_going_contestant`, `minimum_score`
 -- Table structure for table `scores`
 --
 
-CREATE TABLE IF NOT EXISTS `scores` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `scores` (
+  `id` int(10) NOT NULL,
   `judge_id` int(10) NOT NULL,
   `contestant_id` int(10) NOT NULL,
   `criteria_id` int(10) NOT NULL,
-  `score` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `judge_id` (`judge_id`,`criteria_id`),
-  KEY `criteria_id` (`criteria_id`),
-  KEY `contestant_id` (`contestant_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `score` int(10) UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -160,15 +143,103 @@ CREATE TABLE IF NOT EXISTS `scores` (
 -- Table structure for table `winners`
 --
 
-CREATE TABLE IF NOT EXISTS `winners` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `winners` (
+  `id` int(10) NOT NULL,
   `contestant_id` int(10) NOT NULL,
   `overall_score` decimal(10,2) NOT NULL,
-  `place` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `contestat_id` (`contestant_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `place` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `consolation_prizes`
+--
+ALTER TABLE `consolation_prizes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `contestant_id` (`contestant_id`);
+
+--
+-- Indexes for table `contestants`
+--
+ALTER TABLE `contestants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `criteria`
+--
+ALTER TABLE `criteria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `judges`
+--
+ALTER TABLE `judges`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `preferences`
+--
+ALTER TABLE `preferences`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `judge_id` (`judge_id`,`criteria_id`),
+  ADD KEY `criteria_id` (`criteria_id`),
+  ADD KEY `contestant_id` (`contestant_id`);
+
+--
+-- Indexes for table `winners`
+--
+ALTER TABLE `winners`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `contestat_id` (`contestant_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `consolation_prizes`
+--
+ALTER TABLE `consolation_prizes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `contestants`
+--
+ALTER TABLE `contestants`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `criteria`
+--
+ALTER TABLE `criteria`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `judges`
+--
+ALTER TABLE `judges`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `preferences`
+--
+ALTER TABLE `preferences`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `winners`
+--
+ALTER TABLE `winners`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
